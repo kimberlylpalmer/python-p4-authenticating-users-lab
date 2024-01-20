e0fc823bef93fbb45408f5194adeeeab198ada0f
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-01-20 14:06:58 -0800

    lab complete

--- server/app.py
+++ server/app.py
@@ -18,6 +18,12 @@ db.init_app(app)
 
 api = Api(app)
 
+
+@app.route('/')
+def home():
+    return "Welcome to the API!"
+
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -51,6 +57,38 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
+class Login(Resource):
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        
+        session['user_id'] = None
+        
+        return {}, 204
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+        
+        
 
 
 if __name__ == '__main__':
